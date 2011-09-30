156f9e292f5dddb46d09fd31ac423509194d0301
snw <donchoa@gmail.com>
2011-09-30 12:04:49 -0400

    newwin testing

--- ncurse-tut/ncurse-tut/main.cpp
+++ ncurse-tut/ncurse-tut/main.cpp
@@ -13,8 +13,27 @@ using std::string;
 
 int main (int argc, const char * argv[])
 {
-
     initscr();
+    noecho();
+    raw();
+    
+    // enable the arrow keys and F functions
+    keypad(stdscr, TRUE);
+    // hide the cursor
+    // curs_set(0);
+    
+    WINDOW* wlegend = newwin(1, getmaxx(stdscr), getmaxy(stdscr)-1, 0);
+    // box(wlegend, 0, 0);
+    // box(stdscr, 0, 0);
+    
+    mvprintw(getmaxy(stdscr)-2, getmaxx(stdscr)-1, "b");
+    refresh();
+    
+    mvwprintw(wlegend, getmaxy(wlegend)-1, getmaxx(wlegend)-1, "a");
+    //mvwprintw(wlegend, 0, 0, "The wlegend height: %d, width: %d.", getmaxy(wlegend), getmaxx(wlegend));
+    wrefresh(wlegend);
+    
+    getch();
     endwin();
     
     return 0;
